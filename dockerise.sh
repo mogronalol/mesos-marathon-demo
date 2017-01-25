f2080564316c24c51fb3a02a99a2f7feb3b80f3e
Andrew Morgan <andrew.morgan@opencredo.com>
2017-01-25 23:10:55 +0000

    No longer push latest tag

--- dockerise.sh
+++ dockerise.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
+set -e
 docker build -t mogronalol/mesos-marathon-demo:$BUILD_NUMBER .
 docker push mogronalol/mesos-marathon-demo:$BUILD_NUMBER
-docker push mogronalol/mesos-marathon-demo:latest
