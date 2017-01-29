e82a3fed3c4f4fe1faa9beb5fa0b4ba06ad5b45f
Andrew Morgan <andrew.morgan@opencredo.com>
2017-01-29 12:24:05 +0000

    Added some loggin

--- dockerise.sh
+++ dockerise.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 set -e
+set -v
+echo $DOCKER_PASSWORD
 docker login -u mogronalol -p $DOCKER_PASSWORD
 docker build -t mogronalol/mesos-marathon-demo:$BUILD_NUMBER .
 docker push mogronalol/mesos-marathon-demo:$BUILD_NUMBER
