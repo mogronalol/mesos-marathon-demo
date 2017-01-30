e3d00be5c113e65d8c8d2073e786133a1fd41fc9
Andrew Morgan <andrew.morgan@opencredo.com>
2017-01-30 23:54:15 +0000

    Updated DockerFile

--- dockerise.sh
+++ dockerise.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 set -e
-set -v
-echo $DOCKER_PASSWORD
 docker login -u mogronalol -p $DOCKER_PASSWORD
 docker build -t mogronalol/mesos-marathon-demo:$BUILD_NUMBER .
 docker push mogronalol/mesos-marathon-demo:$BUILD_NUMBER
